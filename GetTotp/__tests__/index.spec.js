e4c9b56edf915c79b3ccd9195b71a30bf93744fd
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-07-23 12:01:43 +0100

    Fix bad naming
    Thx Ganesh to point this out !

--- GetTotp/__tests__/index.spec.js
+++ GetTotp/__tests__/index.spec.js
@@ -1,5 +1,5 @@
 const mockAxios = require("axios");
-const getS2SToken = require("../index");
+const getTotp = require("../index");
 
 const validHeaders = {
   "service-key": "the-key"
@@ -13,7 +13,7 @@ describe("GetTotp function", () => {
   it("should return an error response when the header parameters are invalid", async () => {
     const done = jest.fn();
     const ctx = { done };
-    await getS2SToken(ctx, { headers: {} });
+    await getTotp(ctx, { headers: {} });
 
     expect(ctx.res.status).toEqual(500);
   });
@@ -22,7 +22,7 @@ describe("GetTotp function", () => {
     const done = jest.fn();
     const ctx = { done };
     const req = { headers: validHeaders };
-    await getS2SToken(ctx, req);
+    await getTotp(ctx, req);
 
     expect(ctx.res.status).toEqual(200);
     expect(ctx.res.body.totp).toMatch(/[0-9\(\)]+/);
